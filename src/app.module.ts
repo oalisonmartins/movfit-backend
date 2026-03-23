c54a71b85eb4ae5749b70f009142d39217c4f06a
Alison G. Martins <odevalison@gmail.com>
2026-03-23 15:16:29 +0000

    feat: add healthcheck endpoint (#20)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,14 @@
-import { Module } from '@nestjs/common';
-
-import { UsersModule } from './modules/users/users.module';
-import { AuthModule } from './modules/auth/auth.module';
-import { DailyNutritionModule } from './modules/daily-nutrition/daily-nutrition.module';
-import { WaterIngestionModule } from './modules/water-ingestion/water-ingestion.module';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { AuthModule } from './modules/auth/auth.module'
+import { DailyNutritionModule } from './modules/daily-nutrition/daily-nutrition.module'
+import { UsersModule } from './modules/users/users.module'
+import { WaterIngestionModule } from './modules/water-ingestion/water-ingestion.module'
 
 @Module({
-  imports: [
-    UsersModule,
-    AuthModule,
-    DailyNutritionModule,
-    WaterIngestionModule,
-  ],
+  controllers: [AppController],
+  imports: [UsersModule, AuthModule, DailyNutritionModule, WaterIngestionModule],
+  providers: [AppService],
 })
 export class AppModule {}
