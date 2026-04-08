e371a1c49999b610919c3f9c2e6e09d6b2c250ca
Alison Gabriel <odevalison@gmail.com>
2026-04-08 00:32:17 -0300

    refactor: remove water consumption history module

--- src/modules/water-consumption-history/helpers/parse-date.helper.ts
+++
@@ -1,6 +0,0 @@
-type DateParts = [day: string, month: string, year: string]
-
-export const parseDate = (value: string): Date => {
-  const [day, month, year] = value.split('/') as DateParts
-  return new Date(Date.UTC(+year, +month - 1, +day))
-}
--- src/modules/water-consumption-history/types/get-water-consumption-history.type.ts
+++
@@ -1,4 +0,0 @@
-export type GetWaterConsumptionHistoryRequest = {
-  fromDate: Date
-  toDate: Date
-}
