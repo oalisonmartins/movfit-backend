8f89bca2e66fa6298b7335459b8e88b82f378662
Alison Gabriel <odevalison@gmail.com>
2026-04-19 00:01:45 -0300

    feat: update diets module with new use cases and dependencies

--- src/modules/diets/diets.module.ts
+++ src/modules/diets/diets.module.ts
@@ -1,13 +1,19 @@
 import { Module } from '@nestjs/common'
 import { RequestContextService } from 'src/common/services/request-context.service'
 import { PrismaService } from 'src/infra/database/prisma/prisma.service'
+import { FoodsModule } from '../foods/foods.module'
+import { MealsModule } from '../meals/meals.module'
+import { ProfileModule } from '../profile/profile.module'
 import { DietsController } from './controller/diets.controller'
 import { DietsRepository } from './repositories/diets.repository'
 import { PrismaDietsRepository } from './repositories/prisma-diets.repository'
+import { CreateDietUseCase } from './use-cases/create-diet.use-case'
+import { DeleteDietUseCase } from './use-cases/delete-diet.use-case'
 import { GetActiveDietUseCase } from './use-cases/get-active-diet.use-case'
 import { GetDietsUseCase } from './use-cases/get-diets.use-case'
 
 @Module({
+  imports: [MealsModule, FoodsModule, ProfileModule],
   controllers: [DietsController],
   exports: [DietsRepository],
   providers: [
@@ -15,6 +21,8 @@ import { GetDietsUseCase } from './use-cases/get-diets.use-case'
     RequestContextService,
     GetDietsUseCase,
     GetActiveDietUseCase,
+    CreateDietUseCase,
+    DeleteDietUseCase,
     {
       provide: DietsRepository,
       useClass: PrismaDietsRepository,
