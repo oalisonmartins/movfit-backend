03a9ee21ac0d44a0bc698dbbf7fffdf4a7ab8d8c
Alison Gabriel <odevalison@gmail.com>
2026-04-19 01:56:08 -0300

    refactor: update Diets repository abstract methods with TransactionClient parameter

--- src/modules/diets/repositories/diets.repository.ts
+++ src/modules/diets/repositories/diets.repository.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@nestjs/common'
 import { Diet } from 'generated/prisma/client'
+import { TransactionClient } from 'generated/prisma/internal/prismaNamespace'
 import { CreateDietRepositoryInput } from '../types/create-diet.types'
 
 @Injectable()
 export abstract class DietsRepository {
   abstract getOne(dietId: string, userId: string): Promise<Diet | null>
   abstract getAll(userId: string): Promise<Diet[]>
-  abstract getActive(userId: string): Promise<Diet | null>
-  abstract create(userId: string, input: CreateDietRepositoryInput): Promise<Diet>
-  abstract deactive(dietId: string, userId: string): Promise<void>
-  abstract delete(dietId: string): Promise<void>
+  abstract getActive(userId: string, tx?: TransactionClient): Promise<Diet | null>
+  abstract create(userId: string, input: CreateDietRepositoryInput, tx?: TransactionClient): Promise<Diet>
+  abstract deactive(dietId: string, userId: string, tx?: TransactionClient): Promise<void>
+  abstract delete(dietId: string, userId: string): Promise<void>
 }
