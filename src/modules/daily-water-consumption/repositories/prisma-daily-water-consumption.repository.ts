557a85a55e7941ba7f2395ceed26184b2d64bcaa
Alison Gabriel <odevalison@gmail.com>
2026-04-19 01:56:01 -0300

    refactor: update DailyWaterConsumption repository to extend BaseRepository

--- src/modules/daily-water-consumption/repositories/prisma-daily-water-consumption.repository.ts
+++ src/modules/daily-water-consumption/repositories/prisma-daily-water-consumption.repository.ts
@@ -1,28 +1,28 @@
 import { Injectable } from '@nestjs/common'
 import { DailyWaterConsumption } from 'generated/prisma/client'
+import { BaseRepository } from 'src/common/repositories/base.repository'
+import { TransactionContextService } from 'src/common/services/transaction-context.service'
 import { PrismaService } from 'src/infra/database/prisma/prisma.service'
 import { UpsertDailyWaterConsumptionInput } from '../types/upsert-consumption.type'
 import { DailyWaterConsumptionRepository } from './daily-water-consumption.repository'
 
 @Injectable()
-export class PrismaDailyWaterConsumptionRepository implements DailyWaterConsumptionRepository {
-  constructor(private readonly prisma: PrismaService) {}
-
-  async get(userId: string): Promise<DailyWaterConsumption | null> {
-    const dailyWaterConsumption = await this.prisma.dailyWaterConsumption.findFirst({
-      where: { userId: userId },
-    })
-    if (!dailyWaterConsumption) {
-      return null
-    }
-    return dailyWaterConsumption
+export class PrismaDailyWaterConsumptionRepository extends BaseRepository implements DailyWaterConsumptionRepository {
+  constructor(
+    readonly prisma: PrismaService,
+    readonly transactionContext: TransactionContextService,
+  ) {
+    super(prisma, transactionContext)
   }
 
-  async upsert(
-    userId: string,
-    input: UpsertDailyWaterConsumptionInput,
-  ): Promise<DailyWaterConsumption> {
-    const dailyWaterConsumption = await this.prisma.dailyWaterConsumption.upsert({
+  async get(userId: string): Promise<DailyWaterConsumption | null> {
+    return await this.db.dailyWaterConsumption.findFirst({
+      where: { userId: userId },
+    })
+  }
+
+  async upsert(userId: string, input: UpsertDailyWaterConsumptionInput): Promise<DailyWaterConsumption> {
+    return await this.db.dailyWaterConsumption.upsert({
       where: { userId },
       create: {
         userId,
@@ -34,6 +34,5 @@ export class PrismaDailyWaterConsumptionRepository implements DailyWaterConsumpt
         consumedInMl: { increment: input.totalConsumedInMl },
       },
     })
-    return dailyWaterConsumption
   }
 }
