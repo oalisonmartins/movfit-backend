c2cbd464942d0c8c80eaef236d9e54ad6a3ac7f6
Alison Gabriel <odevalison@gmail.com>
2026-04-19 01:56:15 -0300

    refactor: update PrismaDiets repository to extend BaseRepository

--- src/modules/diets/repositories/prisma-diets.repository.ts
+++ src/modules/diets/repositories/prisma-diets.repository.ts
@@ -1,65 +1,63 @@
 import { Injectable } from '@nestjs/common'
 import { Diet } from 'generated/prisma/client'
+import { BaseRepository } from 'src/common/repositories/base.repository'
+import { TransactionContextService } from 'src/common/services/transaction-context.service'
 import { PrismaService } from 'src/infra/database/prisma/prisma.service'
 import { CreateDietRepositoryInput } from '../types/create-diet.types'
 import { DietsRepository } from './diets.repository'
 
 @Injectable()
-export class PrismaDietsRepository implements DietsRepository {
-  constructor(private readonly prisma: PrismaService) {}
+export class PrismaDietsRepository extends BaseRepository implements DietsRepository {
+  constructor(
+    readonly prisma: PrismaService,
+    readonly transactionContext: TransactionContextService,
+  ) {
+    super(prisma, transactionContext)
+  }
 
   async create(userId: string, input: CreateDietRepositoryInput): Promise<Diet> {
-    const diet = await this.prisma.diet.create({
+    return await this.db.diet.create({
       data: {
-        id: input.dietId,
+        userId,
         totalCaloriesInKcal: input.totalCaloriesInKcal,
         totalProteinsInGrams: input.totalProteinsInGrams,
         totalCarbsInGrams: input.totalCarbsInGrams,
         totalFatsInGrams: input.totalFatsInGrams,
         goal: input.goal,
         isActive: true,
-        userId,
       },
     })
-    return diet
   }
 
   async getOne(dietId: string, userId: string): Promise<Diet | null> {
-    const diet = await this.prisma.diet.findUnique({
+    return await this.db.diet.findFirst({
       where: { id: dietId, userId },
     })
-    if (!diet) return null
-    return diet
   }
 
   async getAll(userId: string): Promise<Diet[]> {
-    const diets = await this.prisma.diet.findMany({
+    return await this.db.diet.findMany({
       where: { userId },
       orderBy: [{ createdAt: 'desc' }],
     })
-    return diets
   }
 
   async getActive(userId: string): Promise<Diet | null> {
-    const activeDiet = await this.prisma.diet.findFirst({
+    return await this.db.diet.findFirst({
       where: { userId, isActive: { equals: true } },
     })
-    if (!activeDiet) {
-      return null
-    }
-    return activeDiet
   }
 
   async deactive(dietId: string, userId: string): Promise<void> {
-    await this.prisma.diet.update({
-      where: { id: dietId, userId },
+    await this.db.diet.update({
+      where: { id: dietId, userId, isActive: { equals: true } },
       data: { isActive: false },
     })
   }
 
-  async delete(dietId: string): Promise<void> {
-    await this.prisma.diet.delete({
-      where: { id: dietId },
+  async delete(dietId: string, userId: string): Promise<void> {
+    await this.db.diet.delete({
+      where: { id: dietId, userId },
     })
   }
 }
