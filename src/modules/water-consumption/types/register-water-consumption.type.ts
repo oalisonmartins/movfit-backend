ce8cac6dfe212fb44a9610d5c08b82399a3da506
Alison Gabriel <odevalison@gmail.com>
2026-04-24 21:05:02 -0300

    chore: remove deprecated dtos and types files

--- src/modules/water-consumption/types/register-water-consumption.type.ts
+++
@@ -1,9 +0,0 @@
-export type RegisterWaterConsumptionRequest = {
-  amountConsumedInMl: number
-}
-
-export type RegisterWaterConsumptionInput = {
-  dailyWaterConsumptionId: string
-  dateOfConsumption: Date
-  amountConsumedInMl: number
-}
