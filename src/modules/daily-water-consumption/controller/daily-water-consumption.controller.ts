ea409bd81750026026da0eb4954a4230132e1ce5
Alison Gabriel <odevalison@gmail.com>
2026-04-24 21:03:57 -0300

    refactor: update daily water consumption controller from PATCH to GET endpoint

--- src/modules/daily-water-consumption/controller/daily-water-consumption.controller.ts
+++ src/modules/daily-water-consumption/controller/daily-water-consumption.controller.ts
@@ -1,25 +1,22 @@
-import { Controller, HttpCode, HttpStatus, Patch, UseGuards, UseInterceptors } from '@nestjs/common'
+import { Controller, Get, UseGuards, UseInterceptors } from '@nestjs/common'
 import { RequireProfile } from 'src/common/decorators/require-profile.decorator'
 import { RequireWorkoutConfig } from 'src/common/decorators/require-workout-config.decorator'
 import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard'
 import { OnboardingGuard } from 'src/common/guards/onboarding.guard'
 import { ProfileInterceptor } from 'src/common/interceptors/profile.interceptor'
 import { WorkoutConfigInterceptor } from 'src/common/interceptors/workout-config.interceptor'
-import { CalculateDailyWaterConsumptionUseCase } from '../use-cases/calculate-daily-water-consumption.use-case'
+import { GetDailyWaterConsumptionUseCase } from '../use-cases/get-daily-water-consumption.use-case'
 
 @UseGuards(JwtAuthGuard, OnboardingGuard)
+@UseInterceptors(WorkoutConfigInterceptor, ProfileInterceptor)
 @Controller({ path: '/daily-water-consumption', version: '1' })
 export class DailyWaterConsumptionController {
-  constructor(
-    private readonly calculateDailyWaterConsumptionUseCase: CalculateDailyWaterConsumptionUseCase,
-  ) {}
+  constructor(private readonly getDailyWaterConsumptionUseCase: GetDailyWaterConsumptionUseCase) {}
 
+  @Get()
   @RequireWorkoutConfig()
   @RequireProfile()
-  @UseInterceptors(WorkoutConfigInterceptor, ProfileInterceptor)
-  @Patch()
-  @HttpCode(HttpStatus.CREATED)
-  calculateDailyWaterConsumption() {
-    return this.calculateDailyWaterConsumptionUseCase.execute()
+  getDailyWaterConsumption() {
+    return this.getDailyWaterConsumptionUseCase.execute()
   }
 }
