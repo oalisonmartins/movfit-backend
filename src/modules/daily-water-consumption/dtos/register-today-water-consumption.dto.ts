5caf5d2e59342541918d4a67079572f68d742c1c
Alison Gabriel <odevalison@gmail.com>
2026-04-26 02:49:19 -0300

    chore: remove obsolete daily water consumption dtos

--- src/modules/daily-water-consumption/dtos/register-today-water-consumption.dto.ts
+++
@@ -1,8 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger'
-import { IsInt } from 'class-validator'
-
-export class RegisterTodayWaterConsumptionDto {
-  @ApiProperty({ type: 'integer' })
-  @IsInt()
-  readonly amountConsumedInMl: number
-}
