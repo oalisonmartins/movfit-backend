cdd950741af8f64e3fbfa8891155fd4b124b6afb
Alison Gabriel <odevalison@gmail.com>
2026-05-17 03:31:48 -0300

    chore: include forbidNonWhitelisted to globalPipes config

--- src/main.ts
+++ src/main.ts
@@ -21,8 +21,9 @@ async function bootstrap() {
 
   app.useGlobalPipes(
     new ValidationPipe({
-      transform: true,
       whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
     }),
   )
 
