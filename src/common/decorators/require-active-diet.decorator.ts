6d74c479c611ec077aeceadd4438bfeb2101320a
Alison Gabriel <odevalison@gmail.com>
2026-06-04 23:30:27 -0300

    refactor: add key to RequireActiveDiet decorator

--- src/common/decorators/require-active-diet.decorator.ts
+++ src/common/decorators/require-active-diet.decorator.ts
@@ -1,5 +1,6 @@
 import { SetMetadata } from '@nestjs/common'
+import { constants } from 'src/common/constants'
 
 export const RequireActiveDiet = () => {
-  return SetMetadata('REQUIRE_ACTIVE_DIET', true)
+  return SetMetadata(constants.REQUIRE_ACTIVE_DIET_KEY, true)
 }
