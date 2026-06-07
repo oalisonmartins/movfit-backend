d255a14303a9f65b2d158dbd3dd1ea4229d2a919
Alison Gabriel <odevalison@gmail.com>
2026-06-07 10:56:53 -0300

    fix: add onboarding guard to profiles controller

--- src/modules/profiles/controller/profiles.controller.ts
+++ src/modules/profiles/controller/profiles.controller.ts
@@ -2,11 +2,12 @@ import { Body, Controller, HttpCode, HttpStatus, Patch, UseGuards } from '@nestj
 import { ApiCreatedResponse } from '@nestjs/swagger'
 import { Throttle } from '@nestjs/throttler/dist'
 import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard'
+import { OnboardingGuard } from 'src/common/guards/onboarding.guard'
 import { ProfileDto } from 'src/modules/profiles/dtos/profile.dto'
 import { UpdateProfileDto } from 'src/modules/profiles/dtos/update-profile.dto'
 import { UpdateProfileUseCase } from 'src/modules/profiles/use-cases/update-profile.use-case'
 
-@UseGuards(JwtAuthGuard)
+@UseGuards(JwtAuthGuard, OnboardingGuard)
 @Controller('profile')
 export class ProfilesController {
   constructor(private readonly updateProfileUseCase: UpdateProfileUseCase) {}
